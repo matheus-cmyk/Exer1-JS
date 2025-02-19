1cc229db67ee297ef3a871fee8a8dbeaafbc3f20
Matheus Munhoz <135167128+matheus-cmyk@users.noreply.github.com>
2025-02-19 17:48:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -34,10 +34,82 @@ const rl = readline.createInterface({
 });
 rl.question("Quanto dinheiro você tem na carteira (R$)?", (reais) => {
   let cotacao = parseInt(reais) * 5.6;
-  console.log(`vc tem na sua carteira: R$ ${reais} e em dolar ${cotacao}`);
+  console.log(`voce tem na sua carteira: R$ ${reais} e em dolar ${cotacao}`);
   rl.close();
 });
- seu salário atual:"));
-const aumento = salarioAtual * 0.15;  
-const novoSalario = salarioAtual + aumento;
-console.log(`Seu novo salário, com 15% de aumento, será R$${novoSalario.toFixed(2)}.`);
+ //8 
+//8. Faça um algoritmo que leia a largura e altura de uma parede e mostre a área a ser pintada e a quantidade de tinta necessária para o serviço, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question("Digite a largura da parede (m): ", (largura) => {
+  rl.question("Digite a altura da parede (m): ", (altura) => {
+    let larg = parseFloat(largura);
+    let alt = parseFloat(altura);
+
+    if (isNaN(larg) || isNaN(alt) || larg <= 0 || alt <= 0) {
+      console.log("Por favor, insira valores válidos para largura e altura.");
+    } else {
+      let area = larg * alt;
+      let tinta = area / 2;
+      console.log(
+        `A área da parede é ${area.toFixed(
+          2
+        )}m². Você precisará de ${tinta.toFixed(2)} litros de tinta.`
+      );
+    }
+
+    rl.close();
+  });
+});
+
+//9
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const salarioHora = 25;
+const horasPorDia = 8;
+
+rl.question("Digite o número de dias trabalhados no mês: ", (dias) => {
+  let diasTrabalhados = parseInt(dias);
+
+  if (isNaN(diasTrabalhados) || diasTrabalhados < 0) {
+    console.log("Por favor, insira um número válido de dias trabalhados.");
+  } else {
+    let salarioMensal = diasTrabalhados * horasPorDia * salarioHora;
+    console.log(`O salário do funcionário será R$${salarioMensal.toFixed(2)}.`);
+  }
+
+  rl.close();
+});
+
+//10
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Digite o salário atual do funcionário: ", (salario) => {
+  let salarioAtual = parseFloat(salario);
+
+  if (isNaN(salarioAtual) || salarioAtual < 0) {
+    console.log("Por favor, insira um valor válido para o salário.");
+  } else {
+    let novoSalario = salarioAtual * 1.15;
+    console.log(
+      `O novo salário com 15% de aumento será R$${novoSalario.toFixed(2)}.`
+    );
+  }
+
+  rl.close();
+});
+
